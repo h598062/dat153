9719b78a124e97e01d8ea729c501fde4fe9684ea
Bjørnar <598062@stud.hvl.no>
2025-01-27 14:25:14 +0100

    lagt til mer stoof

--- quizapp/app/src/main/java/no/hvl/dat153/quizapp/MainActivity.java
+++ quizapp/app/src/main/java/no/hvl/dat153/quizapp/MainActivity.java
@@ -1,6 +1,10 @@
 package no.hvl.dat153.quizapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import no.hvl.dat153.quizapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        var poopies = this;
+
+        //SeekBar bar = findViewById(R.id.quiz_words_selector_bar);
+
+        var listener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("ProgressBar", "onProgressChanged: " + progress);
+                Toast.makeText(poopies, "Du har valgt " + progress + " spørsmål", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("ProgressBar", "onStartTrackingTouch: ");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("ProgressBar", "onStopTrackingTouch: ");
+            }
+        };
+
+        //bar.setOnSeekBarChangeListener(listener);
+
+        binding.quizWordsSelectorBar.setOnSeekBarChangeListener(listener);
     }
 }
